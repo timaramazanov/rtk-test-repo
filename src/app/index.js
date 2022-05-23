fe5aae4fbdac5c123ed7f99f4f46da9eb0303df5
timram <rjckec@gmail.com>
2022-05-24 01:00:11 +0400

    init commit

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Ты в master ветке, проверь другие ветки </h1>
+    </div>
+  );
+}
+
+export default App;
