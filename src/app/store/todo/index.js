da4c9d6a33278bce3baeb4186063e17c4db6c5ca
timram <rjckec@gmail.com>
2022-05-24 01:38:30 +0400

    replace createAction/createReducer with createSlice

--- src/app/store/todo/index.js
+++ src/app/store/todo/index.js
@@ -1,46 +1,54 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
 
 const initState = {
   todos: []
 }
 
-export const addTodo = createAction('todo/add')
-export const updateTodo = createAction('todo/update')
-export const finishTodo = createAction('todo/finish')
-export const activateTodo = createAction('todo/activate')
-export const removeTodo = createAction('todo/remove')
-
-export const todosReducer = createReducer(initState, {
-  [addTodo]: (state, action) => {
-    state.todos.push({
-      task: action.payload.task,
-      finished: false,
-      id: nanoid()
-    })
-  },
-  [updateTodo]: (state, action) => {
-    state.todos = state.todos.map(
-      t => t.id === action.payload.id
-        ? { ...t, task: action.payload.task }
-        : t
-    )
-  },
-  [finishTodo]: (state, action) => {
-    state.todos = state.todos.map(
-      t => t.id === action.payload.id
-        ? { ...t, finished: true }
-        : t
-    )
-  },
-  [activateTodo]: (state, action) => {
-    state.todos = state.todos.map(
-      t => t.id === action.payload.id
-        ? { ...t, finished: false }
-        : t
-    )
-  },
-  [removeTodo]: (state, action) => {
-    state.todos = state.todos.filter(t => t.id !== action.payload.id)
+const todosSlice = createSlice({
+  name: 'todo',
+  initialState: initState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push({
+        task: action.payload.task,
+        finished: false,
+        id: nanoid()
+      })
+    },
+    updateTodo: (state, action) => {
+      state.todos = state.todos.map(
+        t => t.id === action.payload.id
+          ? { ...t, task: action.payload.task }
+          : t
+      )
+    },
+    finishTodo: (state, action) => {
+      state.todos = state.todos.map(
+        t => t.id === action.payload.id
+          ? { ...t, finished: true }
+          : t
+      )
+    },
+    activateTodo: (state, action) => {
+      state.todos = state.todos.map(
+        t => t.id === action.payload.id
+          ? { ...t, finished: false }
+          : t
+      )
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter(t => t.id !== action.payload.id)
+    }
   }
 });
+
+export const {
+  addTodo,
+  updateTodo,
+  finishTodo,
+  activateTodo,
+  removeTodo
+} = todosSlice.actions;
+
+export const todosReducer = todosSlice.reducer;
