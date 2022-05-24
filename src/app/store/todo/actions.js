69971664a5a05e2b45d8313eb2b6bbe4eafa37f1
timram <rjckec@gmail.com>
2022-05-25 02:07:15 +0400

    implemented using raw redux approach

---
+++ src/app/store/todo/actions.js
@@ -0,0 +1,9 @@
+export const ADD_TODO = 'todo/add';
+
+export const UPDATE_TODO = 'todo/update';
+
+export const FINISH_TODO = 'todo/finish';
+
+export const ACTIVATE_TODO = 'todo/activate';
+
+export const REMOVE_TODO = 'todo/remove';
--- src/app/store/todo/index.js
+++ src/app/store/todo/index.js
@@ -1,46 +1,61 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import {
+  ADD_TODO,
+  UPDATE_TODO,
+  FINISH_TODO,
+  ACTIVATE_TODO,
+  REMOVE_TODO
+} from './actions';
 import { nanoid } from 'nanoid';
 
 const initState = {
   todos: []
 }
 
-export const addTodo = createAction('todo/add')
-export const updateTodo = createAction('todo/update')
-export const finishTodo = createAction('todo/finish')
-export const activateTodo = createAction('todo/activate')
-export const removeTodo = createAction('todo/remove')
+export const addTodo = payload => ({ type: ADD_TODO, payload })
+export const updateTodo = payload => ({ type: UPDATE_TODO, payload })
+export const finishTodo = payload => ({ type: FINISH_TODO, payload })
+export const activateTodo = payload => ({ type: ACTIVATE_TODO, payload })
+export const removeTodo = payload => ({ type: REMOVE_TODO, payload })
 
-export const todosReducer = createReducer(initState, {
-  [addTodo]: (state, action) => {
-    state.todos.push({
-      task: action.payload.task,
-      finished: false,
-      id: nanoid()
-    })
-  },
-  [updateTodo]: (state, action) => {
-    state.todos = state.todos.map(
-      t => t.id === action.payload.id
-        ? { ...t, task: action.payload.task }
-        : t
-    )
-  },
-  [finishTodo]: (state, action) => {
-    state.todos = state.todos.map(
-      t => t.id === action.payload.id
-        ? { ...t, finished: true }
-        : t
-    )
-  },
-  [activateTodo]: (state, action) => {
-    state.todos = state.todos.map(
-      t => t.id === action.payload.id
-        ? { ...t, finished: false }
-        : t
-    )
-  },
-  [removeTodo]: (state, action) => {
-    state.todos = state.todos.filter(t => t.id !== action.payload.id)
+export const todosReducer = (state = initState, action) => {
+  switch(action.type) {
+    case ADD_TODO:
+      return {
+        todos: state.todos.concat({
+          task: action.payload.task,
+          finished: false,
+          id: nanoid()
+        })
+      }
+    case UPDATE_TODO:
+      return {
+        todos: state.todos.map(
+          t => t.id === action.payload.id
+            ? { ...t, task: action.payload.task }
+            : t
+        )
+      }
+    case FINISH_TODO:
+      return {
+        todos: state.todos.map(
+          t => t.id === action.payload.id
+            ? { ...t, finished: true }
+            : t
+        )
+      }
+    case ACTIVATE_TODO:
+      return {
+        todos: state.todos.map(
+          t => t.id === action.payload.id
+            ? { ...t, finished: false }
+            : t
+        )
+      }
+    case REMOVE_TODO:
+      return {
+        todos: state.todos.filter(t => t.id !== action.payload.id)
+      }
+    default:
+      return state
   }
-});
+}
